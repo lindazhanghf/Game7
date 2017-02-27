cd6e63fe9ab003b7e24101d570d20e6a00b2405c
zzhang391 <zzhang391@gatech.edu>
2017-02-27 00:04:02 -0500

    Add change_color()

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+    public bool isWhite = true; //  true for white, false for black
+    public Material white;
+    public Material black;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = transform.parent.FindChild("Player").gameObject;
+    }
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.ToString() + " entered the trigger");
+        if (other.gameObject == player)
+        {
+            player.GetComponent<PlayerController>().touch_object(transform.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.ToString() + " exited the trigger");
+        if (other.gameObject == player)
+        {
+            player.GetComponent<PlayerController>().leave_object(transform.gameObject);
+        }
+    }
+
+    public void change_color()
+    {
+        isWhite = !isWhite;
+        if (isWhite)
+        {
+            GetComponent<MeshRenderer>().material = white;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material = black;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isArrowKey = false;
+    public List<GameObject> objects;
+
+    //public float jumpSpeed = 8.0F;
+    //public float gravity = 20.0F;
+    public float speed = 6.0F;
+    public float rotate_speed = 80.0F;
+    private float forward = 0F;
+    private float rotation = 0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Update()
+    {
+        movement();
+        if ((isArrowKey && Input.GetKeyDown(KeyCode.Return)) || (!isArrowKey && Input.GetKeyDown(KeyCode.Space)))
+        {
+            foreach (GameObject obj in objects)
+            {
+                obj.GetComponent<ObjectController>().change_color();
+            }
+        }
+    }
+
+    public void touch_object(GameObject obj)
+    {
+        objects.Add(obj);
+    }
+
+    public void leave_object(GameObject obj)
+    {
+        objects.Remove(obj);
+    }
+
+    void movement()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        //if (controller.isGrounded)
+        //{
+        //    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //    moveDirection = transform.TransformDirection(moveDirection);
+        //    moveDirection *= speed;
+        //    if (Input.GetButton("Jump"))
+        //        moveDirection.y = jumpSpeed;
+        //}
+        //moveDirection.y -= gravity * Time.deltaTime;
+        forward = 0F;
+        if ((isArrowKey && Input.GetKey(KeyCode.UpArrow)) || (!isArrowKey && Input.GetKey(KeyCode.W)))
+        {
+            forward += speed;
+        }
+        if ((isArrowKey && Input.GetKey(KeyCode.DownArrow)) || (!isArrowKey && Input.GetKey(KeyCode.S)))
+        {
+            forward -= speed;
+        }
+        moveDirection = new Vector3(forward, 0, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(moveDirection * Time.deltaTime);
+
+        rotation = 0F;
+        if ((isArrowKey && Input.GetKey(KeyCode.LeftArrow)) || (!isArrowKey && Input.GetKey(KeyCode.A)))
+        {
+            rotation -= rotate_speed;
+        }
+        if ((isArrowKey && Input.GetKey(KeyCode.RightArrow)) || (!isArrowKey && Input.GetKey(KeyCode.D)))
+        {
+            rotation += rotate_speed;
+        }
+        transform.Rotate(Vector3.up * rotation * Time.deltaTime, Space.World);
+    }
+}
